4dfb78889276fbbe20d310d99967ebf1447751b6
git1bhay <soniabhay797@gmail.com>
2024-02-18 19:38:25 +0530

    code css3d

--- Js/BaseClass.js
+++ Js/BaseClass.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import {CSS3DRenderer} from 'three/examples/jsm/renderers/CSS3DRenderer';
 
 class BaseScene {
   constructor() {
@@ -18,15 +19,31 @@ class BaseScene {
 
   createRenderer() {
     this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.container.appendChild(this.renderer.domElement);
+
+    this.css3dContainer = document.createElement("div");
+    this.css3dContainer.style.position = 'absolute';
+      // this.css3dContainer.style.width = "50px";
+    // this.css3dContainer.style.height = "50px";
+    this.container.appendChild(this.css3dContainer);
+    
+    this.renderer1 = new CSS3DRenderer();
+    this.renderer1.setSize(
+      this.container.clientWidth,
+      this.container.clientHeight
+    );
+    this.css3dContainer.appendChild(this.renderer1.domElement);
+  
+
   }
 
   createControls() {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.camera.position.set(0, 5, 15);
-    this.controls = new OrbitControls(this.camera, this.container);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls1 = new OrbitControls(this.camera,this.renderer1.domElement);
   }
 
   addEventListeners() {
@@ -45,7 +62,9 @@ class BaseScene {
   render() {
     requestAnimationFrame(this.render.bind(this));
     this.controls.update();
+    this.controls1.update();
     this.renderer.render(this.scene, this.camera);
+    this.renderer1.render(this.scene,this.camera);
   }
 }
 
